e5131e243318a61cb2ed879c8f7d1128b6ab56ea
harycane <harycane@gmail.com>
2020-02-19 22:51:33 -0600

    Adding LCA BST 235

---
+++ Trees/235_LCA_BST.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        while root:
+
+            if p.val < root.val and q.val < root.val:
+                root = root.left
+
+            elif p.val > root.val and q.val > root.val:
+                root = root.right
+
+            else:
+                break
+
+        return root
+
+        # iterative T O(h = n worst case) log n balanced, S O(1)
+
